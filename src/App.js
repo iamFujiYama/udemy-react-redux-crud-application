7910facc64071040db151a72a5ad70809d80f2f0
1kfuji <1kfuji@jp.ibm.com>
2019-01-25 19:18:59 +0900

    learned about components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, {Fragment, Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <label htmlFor="Foo"> Foo </label>
-        <input id="Foo" type="button" onClick={ ()=> {alert('hi !')}} />
-      </Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <Fragment>
+//         <label htmlFor="Foo"> Foo </label>
+//         <input id="Foo" type="button" onClick={ ()=> {alert('hi !')}} />
+//       </Fragment>
+//     )
+//   }
+// }
+
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
+const Cat = () => {
+  return (
+    <div>Nya~</div>
+  )
+}
 
 export default App;
