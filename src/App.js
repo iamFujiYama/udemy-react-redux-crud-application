eae757cdede2fc8284060b5255f0d89aedca45f0
1kfuji <1kfuji@jp.ibm.com>
2019-01-25 19:02:14 +0900

    Leaned about jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React, {Fragment, Component } from 'react';
 
 class App extends Component {
   render() {
-    return ( <div><h1> Hello World </h1></div> );
+    return (
+      <Fragment>
+        <label htmlFor="Foo"> Foo </label>
+        <input id="Foo" type="button" onClick={ ()=> {alert('hi !')}} />
+      </Fragment>
+    )
   }
 }
+
 
 export default App;
